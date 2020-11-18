40234c9ad10e54538383376b4dde54fbaa46fe51
Autumn <autumnstnl@gmail.com>
2020-11-17 23:23:28 -0500

    commit tree f769d5789f6d37445354c4cb1b34e9c7f7fb20a1

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+   if block_given?
+      counter = 0
+
+   while counter < array.length
+      yield(array[counter])
+      counter += 1
+   end
+   array
+  else return
+    "This block should not run!"
+  end
+end
+
+array = [1,2,3,4,5,6]
+my_each(array) do |counter|
+  return array 
 end
